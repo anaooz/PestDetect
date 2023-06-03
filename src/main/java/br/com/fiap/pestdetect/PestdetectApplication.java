7507521f963f5ff560034379deeccf0bf0f7441d
Mateus <mateusmarchetti.vieira@gmail.com>
2023-06-03 12:00:32 -0300

    projeto inicial

---
+++ src/main/java/br/com/fiap/pestdetect/PestdetectApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pestdetect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PestdetectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PestdetectApplication.class, args);
+	}
+
+}
